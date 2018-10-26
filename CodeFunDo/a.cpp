7588faedc5dc1bae8b861dc58a3982f5b16f6518
Bournvita1998 <mohit.kuri@research.iiit.ac.in>
2018-10-26 16:44:55 +0530

    basic update

---
+++ CodeFunDo/a.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i,j,t,n,k;
+	cin >> t;
+	for(int p=0;p<t;p++)
+	{
+		cin >> n >> k;
+		int A[n],count = 0,temp = k;
+		for(i=0;i<n;i++)
+			cin >> A[i];
+		sort(A,A+n);
+		int dayno = 0;
+		for(i=0;i<n;i++)
+		{
+			if(A[i]-dayno > 0 && temp > 0)
+			{
+				count++;
+				temp--;
+			}
+			if(temp == 0)
+			{
+				dayno++;
+				temp = k;
+			}
+		}
+		cout << "Case #" << p+1 << ": " << count << endl;
+	}
+}
